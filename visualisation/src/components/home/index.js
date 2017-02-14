4ed2aa99ad9cc350edb72ab535975378bd09e12f
Iain Johnston <Iain.Johnston@bbc.co.uk>
2017-02-14 11:41:42 +0000

    Add round counter and two lists

--- visualisation/src/components/home/index.js
+++ visualisation/src/components/home/index.js
@@ -4,13 +4,29 @@ import style from './style';
 import Order from '../order';
 
 export default class Home extends Component {
+	constructor() {
+		super();
+
+		this.state.round = 0;
+	}
+
+	incrementRound() {
+		this.setState({round: this.state.round + 1});
+	}
+
 	render() {
 		return (
 			<div class={style.home}>
-				<h1>Home</h1>
-				<p>This is the Home component.</p>
 
-				<Order />
+				<h1>Round in order: {this.state.round}</h1>
+
+				<center>
+					<button class={style.round_incrementer} onClick={e => this.incrementRound()}>Next Round</button>
+				</center>
+
+				<Order float='left' title='Algorithms order'/>
+
+				<Order float='right' title='Real world order'/>
 			</div>
 		);
 	}
