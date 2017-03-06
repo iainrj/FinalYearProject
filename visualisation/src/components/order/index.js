96fc984c12f782bed87fd8e3d773459308651b90
Iain Johnston <Iain.Johnston@bbc.co.uk>
2017-03-06 17:26:27 +0000

    Add logic to update scores for each country per round

--- visualisation/src/components/order/index.js
+++ visualisation/src/components/order/index.js
@@ -1,9 +1,14 @@
 import { h, Component } from 'preact';
 import style from './style';
+import scoreboard from './scoreboard';
 
 import Country from '../country';
 
-const countries = ['Ukraine','Belarus','Azerbaijan','Iceland','Norway','Romania','Armenia','Montenegro','Poland','Greece','Austria', 'Germany','Sweden','France','Russia','Italy','Slovenia','Finland','Spain','Switzerland','Hungary','Malta','Denmark','The Netherlands', 'San Marino','United Kingdom'];
+const performers = ['Ukraine','Belarus','Azerbaijan','Iceland','Norway','Romania','Armenia','Montenegro','Poland','Greece','Austria', 'Germany','Sweden','France','Russia','Italy','Slovenia','Finland','Spain','Switzerland','Hungary','Malta','Denmark','The Netherlands', 'San Marino','United Kingdom'];
+
+const voters = ['Albania', 'Armenia', 'Austria', 'Azerbaijan', 'Belarus', 'Belgium', 'Denmark', 'Estonia', 'FYR Macedonia', 'Finland', 'France','Georgia', 'Germany', 'Greece', 'Hungary', 'Iceland', 'Ireland', 'Israel', 'Italy', 'Latvia', 'Lithuania', 'Malta', 'Moldova', 'Montenegro', 'Norway', 'Poland', 'Portugal', 'Romania', 'Russia', 'San Marino', 'Slovenia', 'Spain', 'Sweden', 'Switzerland', 'The Netherlands', 'Ukraine', 'United Kingdom'];
+
+const reveal_order = ['Azerbaijan','Greece','Poland','Albania','San Marino','Denmark','Montenegro','Romania','Russia','The Netherlands','Malta','France','United Kingdom','Latvia','Armenia','Iceland','FYR Macedonia','Sweden','Belarus','Germany','Israel','Portugal','Norway','Estonia','Hungary','Moldova','Ireland','Finland','Lithuania','Austria','Spain','Belgium','Italy','Ukraine','Switzerland','Georgia','Slovenia'];
 
 export default class Order extends Component {
 	render() {
@@ -17,11 +22,22 @@ export default class Order extends Component {
 			<h2>{this.props.title}</h2>
 			<ul class={style.countries_wrapper}>
 			{
-				countries.map((country, index) => {
+				performers.map((country, index) => {
+					let country_scores = [];
+					let c = performers.indexOf(country);
+
+					for (let i = 0; i < voters.length; i++) {
+						country_scores.push(scoreboard[c][i]);
+					}
+
+					const country_index = reveal_order[this.props.round];
+					const score = country_scores[voters.indexOf(country_index)];
+
 					const countryProps = {
 						countryName: country,
 						id: index,
-						round: this.props.round
+						round: this.props.round,
+						newScore: score
 					};
 
 					return (
