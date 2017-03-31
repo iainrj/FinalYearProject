4b6f10da3e8c0e8fe7010660b94574be4fb832e5
Iain Johnston <Iain.Johnston@bbc.co.uk>
2017-03-31 10:24:26 +0100

    Make greedy use iterations since new solution

--- code/greedySearch.py
+++ code/greedySearch.py
@@ -3,31 +3,31 @@ import support
 
 def greedySearch(score_board, countries, voters):
     max_iterations = 100000
-    i = 0
     iters = 0
     xNow = support.getInitialSolution(voters)
     xBest = xNow[:]
-    entertainmentXBest, distances = support.getEntertainment(xBest, countries, score_board, voters, 0)
+    entertainmentXBest, distances = support.getEntertainment(xBest, countries, score_board, voters)
     oldEntertainment = entertainmentXBest
     oldDistances = distances
     
-    while i < max_iterations:
+    for i in range(max_iterations):
         xNow, key1 = support.getAdjacentNeighbour(xNow)
         # entertainmentXNow = getEntertainment(xNow, countries, score_board, voters, key1)
         
-        start = timer()
-        otherE , otherD = support.getEntertainment(xNow, countries, score_board, voters, key1)
-        end = timer()
-        print('full: ', end - start)
+        # start = timer()
+        # otherE , otherD = support.getEntertainment(xNow, countries, score_board, voters)
+        # end = timer()
+        # print('full: ', end - start)
         
-        start = timer()
+        # start = timer()
         entertainmentXNow, distances = support.offsetGetEntertainment(xNow, countries, score_board, voters, key1, oldEntertainment, oldDistances)
-        end = timer()
-        print('reduced: ', end - start)
+        # end = timer()
+        # print('reduced: ', end - start)
         
         if entertainmentXNow < entertainmentXBest:
             print("new solution", entertainmentXNow)
             xBest = xNow[:]
             entertainmentXBest = entertainmentXNow
+            i = 0
         i = i+1
     return "xBest:", xBest, entertainmentXBest
