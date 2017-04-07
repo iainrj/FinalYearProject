153e28c0c89324def15db8959d9e5ab6967288c0
Iain Johnston <Iain.Johnston@bbc.co.uk>
2017-04-07 22:20:47 +0100

    A better way to add scores to bars

--- visualisation/src/components/bar/index.jsx
+++ visualisation/src/components/bar/index.jsx
@@ -3,12 +3,11 @@ import style from './style';
 
 export default function (props) {
 	const transform = `translate(0, ${props.index * props.padding})`;
-	const width = `${props.xScale(props.score) + 10}px`;
-	const x = props.xScale(props.score);
+	const width = `${props.xScale(props.score) + 10}`;
 	return (
 		<g transform={transform}>
 			<rect class={style.chart_rect} width={width} height="20"></rect>
-			<text class={style.chart_text} x={x} y="9.5" dy=".35em">{props.score}</text>
+			<text class={style.chart_text} x={width - 3} y="9.5" dy=".35em">{props.score}</text>
 		</g>
 	);
 }
