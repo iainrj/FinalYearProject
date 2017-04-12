510b8518ec476995629f0b47ac1ae6ea37a3dbfe
Iain Johnston <Iain.Johnston@bbc.co.uk>
2017-04-12 17:28:18 +0100

    fix some tests and more results

--- code/orderTest.py
+++ code/orderTest.py
@@ -1,30 +1,30 @@
 import unittest
-import order
+import support
 
 class MyTest(unittest.TestCase):
     def testGetAdjacentNeighbour(self):
-        self.assertEqual(order.getAdjacentNeighbour([0,1]), ([1,0],0))
-        self.assertIsNot(order.getAdjacentNeighbour(['albania','russia','finland']), ['albania','russia','finland'])
-        self.assertIsNot(order.getAdjacentNeighbour(['albania','russia','finland', 'france', 'italy', 'spain', 'romania', 'germany']), ['albania','russia','finland', 'france', 'italy', 'spain', 'romania', 'germany'])
+        self.assertEqual(support.getAdjacentNeighbour([0,1]), ([1,0],0))
+        self.assertIsNot(support.getAdjacentNeighbour(['albania','russia','finland']), ['albania','russia','finland'])
+        self.assertIsNot(support.getAdjacentNeighbour(['albania','russia','finland', 'france', 'italy', 'spain', 'romania', 'germany']), ['albania','russia','finland', 'france', 'italy', 'spain', 'romania', 'germany'])
     
     def testGetNeighbour(self):
-        self.assertEqual(order.getNeighbour([0,1]), [1,0])
-        self.assertIsNot(order.getAdjacentNeighbour(['albania','russia','finland']), ['albania','russia','finland'])
-        self.assertIsNot(order.getAdjacentNeighbour(['albania','russia','finland', 'france', 'italy', 'spain', 'romania', 'germany']), ['albania','russia','finland', 'france', 'italy', 'spain', 'romania', 'germany'])
+        self.assertEqual(support.getNeighbour([0,1]), [1,0])
+        self.assertIsNot(support.getAdjacentNeighbour(['albania','russia','finland']), ['albania','russia','finland'])
+        self.assertIsNot(support.getAdjacentNeighbour(['albania','russia','finland', 'france', 'italy', 'spain', 'romania', 'germany']), ['albania','russia','finland', 'france', 'italy', 'spain', 'romania', 'germany'])
 
     def testRefinedMaxMin(self):
-        scores = [0,0,1,2,3,5,7,8,10]
-        solution = [0,0,1,2,3,5,7,8,10] # only used for length
-        j = 0
-        self.assertEqual(order.refinedMaxMin(scores, solution, j), 0)
+        scores = [0, 6, 1, 6, 10, 0, 17, 0, 7, 0, 20, 5, 20, 0, 4, 0, 0, 0, 2, 0, 3, 0, 0, 0, 0, 0]
+        solution = [47, 27, 23, 30, 47, 25, 75, 7, 18, 6, 140, 24, 119, 1, 22, 8, 8, 34, 39, 37, 63, 19, 42, 111, 1, 13, 47, 27, 23, 30, 47, 25, 75, 7, 18, 6, 140]
+        j = 1
+        self.assertEqual(support.refinedMaxMin(scores, solution, j, 12), 0)
         
-        scores2 = [0,1,2,3,5,7,8,10,180]
-        j2 = 3
-        self.assertEqual(order.refinedMaxMin(scores2, solution, j2), 10)
+        scores2 = [47, 27, 23, 30, 47, 25, 75, 7, 18, 6, 140, 24, 119, 1, 22, 8, 8, 34, 39, 37, 63, 19, 42, 111, 1, 13]
+        j2 = 16
+        self.assertEqual(support.refinedMaxMin(scores2, solution, j2, 12), 1)
         
-        scores3 = [0,1,2,3,5,26,85,160,180]
-        j3 = 5
-        self.assertEqual(order.refinedMaxMin(scores3, solution, j3), 85)
+        scores3 = [69, 28, 23, 51, 67, 48, 121, 7, 48, 12, 235, 24, 161, 1, 40, 21, 8, 55, 56, 55, 83, 23, 58, 169, 5, 29]
+        j3 = 25
+        self.assertEqual(support.refinedMaxMin(scores3, solution, j3, 12), 83)
         
     # def testOffsetGetEntertainment(self):
     #     xNow = ['albania','russia','finland','france','italy','spain','romania','germany']
@@ -41,24 +41,24 @@ class MyTest(unittest.TestCase):
         
     #     self.assertEqual(order.offsetGetEntertainment(), 4124)
     
-    def testCalculateDistances(self):
-        score_board = [
-            [ 0,  5,  5, 5, 5],
-            [ 1,  0,  3, 1, 0],
-            [ 5,  3,  0, 3, 3],
-            [ 3,  1,  1, 0, 1]
-        ]    
-        #         al  ru  fi  fr es
-        #     al [ 0,  5,  5, 5, 5],
-        #     ru [ 1,  0,  3, 1, 0],
-        #     fi [ 5,  3,  0, 3, 3],
-        #     it [ 3,  1,  1, 0, 1]
-        solution = ['albania','russia','finland','france', 'spain']
-        voters = ['albania','russia','finland', 'france', 'spain']
-        performers = ['albania','russia','finland', 'italy']
-        no_performers = len(performers)
+    # def testCalculateDistances(self):
+    #     score_board = [
+    #         [ 0,  5,  5, 5, 5],
+    #         [ 1,  0,  3, 1, 0],
+    #         [ 5,  3,  0, 3, 3],
+    #         [ 3,  1,  1, 0, 1]
+    #     ]    
+    #     #         al  ru  fi  fr es
+    #     #     al [ 0,  5,  5, 5, 5],
+    #     #     ru [ 1,  0,  3, 1, 0],
+    #     #     fi [ 5,  3,  0, 3, 3],
+    #     #     it [ 3,  1,  1, 0, 1]
+    #     solution = ['albania','russia','finland','france', 'spain']
+    #     voters = ['albania','russia','finland', 'france', 'spain']
+    #     performers = ['albania','russia','finland', 'italy']
+    #     no_performers = len(performers)
         
-        self.assertEqual(order.calculateDistances(solution, voters, no_performers, score_board), [5, 8, 10, 15, 6])
+    #     self.assertEqual(order.calculateDistances(solution, voters, no_performers, score_board), [5, 8, 10, 15, 6])
         
 if __name__ == '__main__':
     unittest.main()
