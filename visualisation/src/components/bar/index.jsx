b11d3f242662eeda6a459b60c337151d6001ebd0
Iain Johnston <Iain.Johnston@bbc.co.uk>
2017-04-27 23:25:39 +0100

    Add country names

--- visualisation/src/components/bar/index.jsx
+++ visualisation/src/components/bar/index.jsx
@@ -9,7 +9,6 @@ export default class Bar extends Component {
 	}
 
 	componentWillReceiveProps(nextProps) {
-		console.log(nextProps.newScore);
 		this.setState({score: this.state.score + nextProps.newScore});
 	}
 
@@ -18,13 +17,18 @@ export default class Bar extends Component {
 	}
 
 	render() {
-		const transform = `translate(0, ${this.props.id * this.props.padding})`;
-		const width = `${this.props.xScale(this.state.score) + 10}`;
+		const transformBar = `translate(0, ${this.props.id * this.props.padding})`;
+		const width = `${+this.props.xScale(this.state.score) + 10}`;
+		const transformName = `translate(${+width+5}, ${this.props.id * this.props.padding})`;
 		return (
-			<g class={style.bar_bar} transform={transform}>
-				<rect class={style.chart_rect} width={width} height="20"></rect>
-				<text class={style.chart_text} x={width - 5} y="9.5" dy=".35em">{this.state.score}</text>
-				{/*<text class={style.chart_text} x={} y="9.5" dy=".35em">{this.props.countryName}</text>*/}
+			<g>
+				<g transform={transformBar}>
+					<rect class={style.chart_rect} width={width} height="20"></rect>
+					<text class={style.chart_text} x={width - 5} y="9.5" dy=".35em">{this.state.score}</text>
+				</g>
+				<g transform={transformName}>
+					<text class={style.chart_text__country} x={0} y="9.5" dy=".35em">{this.props.countryName}</text>
+				</g>
 			</g>
 		);
 	}
