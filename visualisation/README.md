4627613758727c285990231f3b96d831742ad5da
Iain Johnston <Iain.Johnston@bbc.co.uk>
2017-05-04 10:16:58 +0100

    Visualisation readme and about page

---
+++ visualisation/README.md
@@ -0,0 +1,40 @@
+## Development Workflow
+
+**1. Start a live-reload development server:**
+
+```sh
+npm run dev
+```
+
+> This is a full web server nicely suited to your project. Any time you make changes within the `src` directory, it will rebuild and even refresh your browser.
+
+**2. Testing with `mocha`, `karma`, `chai`, `sinon` via `phantomjs`:**
+
+```sh
+npm test
+```
+
+> 🌟 This also instruments the code in `src/` using [isparta](https://github.com/douglasduteil/isparta), giving you pretty code coverage statistics at the end of your tests! If you want to see detailed coverage information, a full HTML report is placed into `coverage/`.
+
+**3. Generate a production build in `./build`:**
+
+```sh
+npm run build
+```
+
+> You can now deploy the contents of the `build` directory to production!
+>
+> **[Surge.sh](https://surge.sh) Example:** `surge ./build -d my-app.surge.sh`
+> 
+> **[Netlify](https://www.netlify.com/docs/cli/) Example:** `netlify deploy`
+>
+> [![Deploy to Netlify](https://www.netlify.com/img/deploy/button.svg)](https://app.netlify.com/start/deploy?repository=https://github.com/developit/preact-boilerplate)
+
+
+**4. Start local production server with [serve](https://github.com/zeit/serve):**
+
+```sh
+npm start
+```
+
+> This is to simulate a production (CDN) server with gzip. It just serves up the contents of `./build`.
--- visualisation/src/components/about/index.jsx
+++ visualisation/src/components/about/index.jsx
@@ -4,9 +4,17 @@ import style from './style';
 export default class About extends Component {
 	render() {
 		return (
-			<div class={style.About}>
+			<div class={style.profile}>
 				<h1>About</h1>
-				<p>This is a project for a BSc in Computer Science final year project</p>
+				<p>This is a project for a BSc in Computer Science final year project at Cardiff University</p>
+				<p>In many elections or competitions, a set of voters will rank a set of candidates from best to worst, or will give scores to some of the candidates, with the winner then being the candidate that gets the highest total number of points. When it comes to revealing the result after all votes have been cast, some competitions proceed by having a roll-call of all the voters in which each announces their own scores.</p>
+				<p>This is often done for entertainment purposes such as in the Eurovision Song Contest. The concept of entertainment, especially with respect to competition, is a heavily subjective subject and as such is hard to quantify in simple terms. There are intuitive constituent parts to an entertaining competition (like Eurovision) such as, if the winner is known early or late, and how many teams are in the running to win. A large constituent part of this project will be to convert these intuitive ideas into mathematical values that can then be maximised by some optimisation algorithm.</p>
+				<p>The two main questions that this project will aim to answer are:</p>
+				<ol>
+					<li>How can we define the concept of ''entertainment'' in the context of an optimisation problem, and hence try to maximise it.</li>
+					<li>In which order should the votes be revealed in order to maximise that entertainment value?</li>
+				</ol>
+				<p>This visualisation's aim is to aid in understanding why a solution to this problem is supposedly entertaining. The visualisation will play out the competition by revealing the votes in the order that the solution deems entertaining. From this it should be able to pinpoint the reasoning behind why the optimisation algorithms reached this solution.</p>
 			</div>
 		);
 	}
